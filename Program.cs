28bab619c5bd6836ed52086de9b1eff56f61a7c4
AQStarLilly <kieranjhislop@gmail.com>
2024-09-13 16:32:33 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordle_Mini_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words =
+            {
+                "APPLE", "HOUSE", "MOUSE", "TRAIN", "PLANE", "BEACH", "CLOUD", "DREAM",
+                "EARTH", "FLAME", "GRAPE", "HEART", "IVORY", "JELLY", "KNIFE", "LEMON",
+                "MUSIC", "NIGHT", "OCEAN", "PIANO", "QUEEN", "RIVER", "STORM", "TABLE",
+                "VOICE", "WATER", "YOUTH", "ZEBRA", "BREAD", "CHAIR", "DANCE", "EAGLE",
+                "FROGS", "GHOST", "HONEY", "JUICE", "KITES", "LIGHT", "MAGIC", "NURSE",
+                "OLIVE", "PAPER", "QUIET", "RADIO", "SNAKE", "TIGER", "UNCLE", "WITCH",
+                "YACHT", "ZESTY", "BLOOM", "CRISP", "DWARF", "ELBOW", "FLUTE", "GLOVE",
+                "HATCH", "IGLOO", "JOKER", "KOALA", "LUNAR", "MANGO", "NOBLE", "OASIS",
+                "PLUSH", "QUILT", "ROBOT", "SWIRL", "TULIP", "URBAN", "VENOM", "WALTZ",
+                "YUMMY", "BLISS", "CRAVE", "DROOP", "EERIE", "FLAKY", "GIDDY", "HOARD",
+                "JAZZY", "KAYAK", "LUCKY", "MOODY", "NERDY", "ONION", "PLUMP", "QUIRK",
+                "ROAST", "SPICY", "TROUT", "UNZIP", "VAGUE", "WHISK", "YIELD", "ACORN",
+                "BROOM", "CLOWN", "DAISY", "FROST", "GREEN", "HAPPY", "JUMBO", "MELON",
+                "NORTH", "PEACH", "QUICK", "SUNNY", "THUMB", "WAGON", "ARCHS", "SCARS",
+                "ARROW", "MARIO", "LUIGI", "UNITY", "YOSHI", "SHELL", "GREEN", "LANDS",
+                "CLASS", "SMITE", "SHIFT", "ENTER", "DANCE", "NOTES", "QUEEN", "GAMES"
+            };
+            //initializes the random word array and selects a secret word                        
+            Random rnd = new Random();
+            int randomWordIndex = rnd.Next(0, words.Length);
+            string secretWord = words[randomWordIndex];
+            int attempts = 1;
+
+            while (true)
+            {
+                //starts the game asking user to choose a word then responds depending on what word was chosen
+                Console.WriteLine("Guess a 5 Letter Word " + secretWord);
+                //converts your word to upper case if you typed it in lower case
+                string input = Console.ReadLine().ToUpper();
+                //if the word user chooses is not 5 digits it will stop you and let you know
+                if (input.Length != 5)
+                {
+                    Console.WriteLine("This word doesn't have 5 letters");
+                }
+                else
+                {
+                    //if secret word is guessed, outputs that you've got it
+                    if (input == secretWord)
+                    {
+                        Console.WriteLine($"You Got it in {attempts}!");
+                        break;
+                    }
+                    else
+                    {
+                        //starts the correction process and will tell you if a letter is correct    
+                        for (int i = 0; i < secretWord.Length; i++)
+                        {
+                            if (secretWord[i] == input[i])
+                            {
+                                Console.WriteLine(input[i]);
+                            }
+                            else if (secretWord.Contains(input[i]))
+                            {
+                                Console.WriteLine("~");
+                            }
+                            else
+                            {
+                                Console.WriteLine("#");
+                            }
+                        }
+                        Console.WriteLine("Guess Again");
+                    }
+                }
+                attempts++;
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WordleMiniGame")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NSCC")]
+[assembly: AssemblyProduct("WordleMiniGame")]
+[assembly: AssemblyCopyright("Copyright © NSCC 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f0a49ae4-f58d-4f91-86a1-3c074103afd1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
